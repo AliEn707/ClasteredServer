c1a05ef2027af1a8a36b51c1adbda60328fc0230
Denis Yarikov <whitesandwitch@yandex.com>
2016-07-24 13:43:05 +0300

    updated info and ruby stripts
    founded strange segfault

--- ruby/slave_echo.rb
+++ ruby/slave_echo.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'io/wait'
 require_relative "clastered_server_packet"
 server = TCPServer.new(12345)
  
@@ -6,25 +7,29 @@ packet=ClasteredServerPacket.new
 
  
 while (connection = server.accept)
-  Thread.new(connection) do |conn|
-    port, host = conn.peeraddr[1,2]
-    client = "#{host}:#{port}"
-    puts "#{client} is connected"
-    begin
-      loop do
-		  packet.init
-		  packet.recv(conn, true)
-		  p packet.parse
-#        line = conn.readline
-#        line = conn.read
-#        puts "#{client} says: #{line}"
-#        conn.write(line)
-#        conn.write(conn.read(1))
-      end
-    rescue EOFError => e
-		p e
-      conn.close
-      puts "#{client} has disconnected"
-    end
-  end
+	Thread.new(connection) do |conn|
+		port, host = conn.peeraddr[1,2]
+		client = "#{host}:#{port}"
+		puts "#{client} is connected"
+		begin
+			loop do
+				if (conn.ready?)
+					packet.init
+					packet.recv(conn, true)
+					p packet.parse
+				end
+				if(Time.now.to_i%2==0 && false)
+					packet.init.set_type(1).add_int(0).set_dest(0,0).send(conn)
+				end#        line = conn.readline
+	#        line = conn.read
+	#        puts "#{client} says: #{line}"
+	#        conn.write(line)
+	#        conn.write(conn.read(1))
+			end
+		rescue EOFError => e
+			p e
+			conn.close
+			puts "#{client} has disconnected"
+		end
+	end
 end
